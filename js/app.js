ad8df86215c940e703dba51d61a94e6230ee2bd3
Jaimin Pandya <jaiminmoslake7020@gmail.com>
2024-11-20 18:39:26 -0800

    correcting js

--- js/app.js
+++ js/app.js
@@ -4,8 +4,3 @@ window.addEventListener('load', () => {
   const app = App();
   app.init();
 });
-
-
-window.addEventListener('load', (event) => {
-  console.log('Page is fully loaded, including images and stylesheets');
-});
