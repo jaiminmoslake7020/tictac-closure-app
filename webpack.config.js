b747f507ad6c216f6f44a53d001cd827cef9fbae
Jaimin Pandya <jaiminmoslake7020@gmail.com>
2024-12-27 23:56:08 -0800

    adding layouts to the app

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,21 @@ const dotenv = require('dotenv');
 // Load environment variables
 const env = dotenv.config().parsed;
 
+console.log('FIREBASE_API_KEY', process.env.FIREBASE_API_KEY);
+
 // Convert environment variables to stringified format for DefinePlugin
-const envKeys = !env.production ? Object.keys(env).reduce((prev, next) => {
+const envKeys = !process.env.FIREBASE_API_KEY ? Object.keys(env).reduce((prev, next) => {
   prev[`process.env.${next}`] = JSON.stringify(env[next]);
   return prev;
-}, {}) : {};
+}, {}) : {
+  'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+  'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+  'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+  'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+  'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+  'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+  'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
+};
 
 console.log('envKeys', envKeys);
 
