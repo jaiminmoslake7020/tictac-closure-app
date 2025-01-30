78f7a79076878ea58eaaab821655ae21c5a6d3e4
Jaimin Pandya <jaiminmoslake7020@gmail.com>
2025-01-30 13:06:21 -0800

    CD pipeline

--- apps/lambda-functions/src/lf-chatgpt-player/scripts/create-api.sh
+++ apps/lambda-functions/src/lf-chatgpt-player/scripts/create-api.sh
@@ -5,32 +5,33 @@ API_ID=$(aws apigatewayv2 create-api \
   --target "arn:aws:lambda:$AWS_REGION:$AWS_ACCOUNT_ID:function:$FUNCTION_NAME" \
   --query "ApiId" --output text)
 
+echo "API Gateway $API_NAME created successfully."
+
 sleep 10
 
 # Add Route
-aws apigatewayv2 create-route \
+ROUTE_ID=$(aws apigatewayv2 create-route \
   --api-id $API_ID \
-  --route-key "$HTTP_METHOD $RESOURCE_PATH"
+  --route-key "$HTTP_METHOD $RESOURCE_PATH" \
+  --query "RouteId" --output text)
+
+echo "API Gateway route $HTTP_METHOD $RESOURCE_PATH created successfully."
 
 sleep 10
 
 # Create Stage
-aws apigatewayv2 create-stage \
+StageName=$(aws apigatewayv2 create-stage \
   --api-id $API_ID \
   --stage-name "default" \
-  --description "Default stage"
+  --description "Default stage" \
+  --query "StageName" --output text)
 
-sleep 10
-
-# Deploy API
-aws apigatewayv2 create-deployment \
-  --api-id $API_ID \
-  --stage-name "default"
+echo "API Gateway stage $StageName created successfully."
 
 sleep 10
 
 # Update CORS configuration
-aws apigatewayv2 update-api \
+ApiEndpoint=$(aws apigatewayv2 update-api \
     --api-id $API_ID \
     --cors-configuration "{
         \"AllowOrigins\": [\"$ALLOWED_ORIGIN\"],
@@ -38,12 +39,44 @@ aws apigatewayv2 update-api \
         \"AllowHeaders\": [\"*\"],
         \"ExposeHeaders\": [\"*\"],
         \"MaxAge\": 3600
-    }"
+    }" \
+    --query "ApiEndpoint" --output text)
 
-# Output API URL
-echo "API URL: https://$API_ID.execute-api.$AWS_ACCOUNT_ID.amazonaws.com"
+echo "API Gateway CORS configuration updated successfully. for $ApiEndpoint"
 
 sleep 10
+
+INTEGRATION_ID=$(aws apigatewayv2 create-integration \
+  --api-id $API_ID \
+  --integration-type AWS_PROXY \
+  --integration-uri arn:aws:lambda:$AWS_REGION:$AWS_ACCOUNT_ID:function:$FUNCTION_NAME \
+  --integration-method GET \
+  --payload-format-version 2.0 \
+  --query "IntegrationId" --output text)
+
+echo "API Gateway integration $INTEGRATION_ID created successfully."
+
+sleep 10
+
+aws apigatewayv2 update-route \
+  --api-id $API_ID \
+  --route-id $ROUTE_ID \
+  --target integrations/$INTEGRATION_ID
+
+echo "API Gateway route $HTTP_METHOD $RESOURCE_PATH updated successfully."
+
+sleep 10
+
+# Deploy API
+DeploymentId=$(aws apigatewayv2 create-deployment \
+  --api-id $API_ID \
+  --stage-name "default" \
+  --query "DeploymentId" --output text)
+
+echo "API Gateway $API_NAME $DeploymentId deployed successfully."
+
+# Output API URL
+echo "API URL: https://$API_ID.execute-api.$AWS_REGION.amazonaws.com"
 
 # add a permission to the Lambda function to allow it to be invoked by the API Gateway
 aws lambda add-permission \
@@ -51,4 +84,9 @@ aws lambda add-permission \
   --statement-id apigateway-allow \
   --action lambda:InvokeFunction \
   --principal apigateway.amazonaws.com \
-  --source-arn "arn:aws:execute-api:$AWS_REGION:$AWS_ACCOUNT_ID:$API_ID/*/$HTTP_METHOD/$RESOURCE_PATH" \
+  --source-arn "arn:aws:execute-api:$AWS_REGION:$AWS_ACCOUNT_ID:$API_ID/*/$HTTP_METHOD$RESOURCE_PATH" \
+
+curl https://$API_ID.execute-api.$AWS_REGION.amazonaws.com/default$RESOURCE_PATH
+
+
+
